04ddbf5818a746aab6f8671d554db8aa52c44af4
ingarskrafts <ingarskrafts@gmail.com>
2025-06-18 04:44:05 -0400

    Step 5: Implement change handlers

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -7,6 +7,14 @@ const FeedbackForm = () => {
         email: '',
         feedback: ''
     });
+
+const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+        ...formData,
+        [name]: value
+    });
+};
   return (
     <>
     <nav>
