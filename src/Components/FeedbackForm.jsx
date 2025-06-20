dcf1ab02c3642e74234317af649bbf5df802c9e8
ingarskrafts <ingarskrafts@gmail.com>
2025-06-20 05:08:42 -0400

    Practice Exercise

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -5,7 +5,8 @@ const FeedbackForm = () => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
-        feedback: ''
+        feedback: '',
+        rating: ''
     });
 
 const handleChange = (event) => {
@@ -22,6 +23,7 @@ const handleSubmit = (event) => {
         Name: ${formData.name}
         Email: ${formData.email}
         Feedback: ${formData.feedback}
+        Rating: ${formData.rating}
     `;
     const isConfirmed = window.confirm(`Please confirm your details:\n\n${confirmationMessage}`);
     if (isConfirmed) {
@@ -29,7 +31,8 @@ const handleSubmit = (event) => {
         setFormData({
             name: '',
             email: '',
-            feedback: ''
+            feedback: '',
+            rating: ''
         });
         alert('Thank you for your valuable feedback!');
     }
@@ -62,6 +65,46 @@ const handleSubmit = (event) => {
             value={formData.feedback}
             onChange={handleChange}
             ></textarea>
+
+<div style={{display:'flex',gap:'10px',flexDirection:'column'}}>
+            <span>Rate Us:</span>
+            <p><input
+                type='radio'
+                name='rating'
+                value={'1'}
+                checked={formData.rating === '1'}
+                onChange={handleChange}
+            /> 1</p>
+            <p><input
+                type='radio'
+                name='rating'
+                value={'2'}
+                checked={formData.rating === '2'}
+                onChange={handleChange}
+            /> 2</p>
+            <p><input
+                type='radio'
+                name='rating'
+                value={'3'}
+                checked={formData.rating === '3'}
+                onChange={handleChange}
+            /> 3</p>
+            <p><input
+                type='radio'
+                name='rating'
+                value={'4'}
+                checked={formData.rating === '4'}
+                onChange={handleChange}
+            /> 4</p>
+            <p><input
+                type='radio'
+                name='rating'
+                value={'5'}
+                checked={formData.rating === '5'}
+                onChange={handleChange}
+            /> 5</p>
+        </div>
+
         <button type='submit'>Submit Feedback</button>
         
       </form>
